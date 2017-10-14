03b3919bd6227b4e669b8b5d91f55aa16ebb75c6
sejutisarkar <sejuti.dancinggirl8@gmail.com>
2017-10-15 01:35:54 +0530

    subs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
- import React from 'react'
- import ReactDOM from 'react-dom'
-//
- import { Button, Form, Grid, Header, Image, Message, Segment ,Icon} from 'semantic-ui-react'
-//
-console.log("sejuti")
- const LoginForm = () => (
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button, Form, Grid, Header, Image, Message, Segment, Icon} from 'semantic-ui-react'
 
-  <div className='login-form'>
+
+
+class Subs extends React.Component{
+  constructor(){
+    super();
+     this.state={log:''}
+     this.update=this.update.bind(this)
+  }
+
+update(e){
+  this.setState({
+    log:e.target.value
+  })
+handleClick = () => this.update()
+
+  render(){
+    return (
+  <div className='sub'>
 
     <style>{`
       body > div,
@@ -21,57 +34,29 @@ console.log("sejuti")
       verticalAlign='middle'
     >
       <Grid.Column style={{ maxWidth: 450 }}>
-
-<<<<<<< HEAD
-        <Header as='h2' color='blue' textAlign='center'>
-          Log-in to your account
+        <Header as='h2' color='teal' textAlign='center' >
+          COMMING SOON
         </Header>
         <Form size='large'>
           <Segment stacked>
             <Form.Input
               fluid
-              icon='user'
+              icon='mail'
               iconPosition='left'
               placeholder='E-mail address'
             />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-            <Button color='blue' fluid size='large' animated=" fade" disabled>
-            <Button.content hidden>
-            <Icon name="right arrow" /></Button.content>
-            <Button.content visible>
-            Login
-            </Button.content>
-            </Button>
+            <Button animated ='fade' color='red' onClick={this.handleClick}>
+     <Button.Content visible>Subscribe</Button.Content>
+     <Button.Content hidden>
+       <Icon name='feed' />
+     </Button.Content>
+   </Button>
           </Segment>
         </Form>
-        <Message>
-          New member? <a href='#'>Sign Up</a>
-        </Message>
       </Grid.Column>
     </Grid>
-    <div>
-    <Grid
-      textAlign='center'
-      style={{ height: '100%' }}
-      verticalAlign='middle'
-    >
-   <Button circular color='facebook' icon='facebook' />
-   <Button circular color='twitter' icon='twitter' />
-   <Button circular color='linkedin' icon='linkedin' />
-   <Button circular color='google plus' icon='google plus' />
-   </Grid>
- </div>
   </div>
 )
- ReactDOM.render(<LoginForm />,document.getElementById("root"))
- export default LoginForm
-=======
-
-ReactDom.render(<App />,document.getElementById("app"))
->>>>>>> 5ad95b56657350a547a8451e179acf9935595cc6
+}}
+ReactDOM.render(<Subs />,document.getElementById('root'))
+export default LoginForm
